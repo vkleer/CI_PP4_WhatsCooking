21cf41f39683104623311fd86521a246b6cd297c
Vilayat Kleer <vkleer@hotmail.com>
2022-09-13 17:57:01 +0000

    Add newline to urls and views file in meal app, update meals page in templates folder with more classes for better layout

--- meal/urls.py
+++ meal/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.MealList.as_view(), name='meals'),
-    path('meal_detail/<meal_id>', 
+    path('meal_detail/<meal_id>',
          views.MealDetail.as_view(), name='meal_detail')
 ]

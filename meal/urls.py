5ed9017e92e4b43a556a5c18163e45b9965ea1fe
Vilayat Kleer <vkleer@hotmail.com>
2022-09-13 15:17:59 +0000

    Create meal_detail page with basic HTML structure, add meal_detail view and url to connect it to each meal listed in meals page

--- meal/urls.py
+++ meal/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.MealList.as_view(), name='meals')
+    path('', views.MealList.as_view(), name='meals'),
+    path('meal_detail/<meal_id>', 
+         views.MealDetail.as_view(), name='meal_detail')
 ]
--- meal/views.py
+++ meal/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Meal
 
 
@@ -8,3 +8,10 @@ class MealList(generic.ListView):
     queryset = Meal.objects.order_by('name')
     template_name = 'meals.html'
     paginate_by = 8
+
+
+class MealDetail(View):
+    def get(self, request, meal_id):
+        meal = Meal.objects.get(pk=meal_id)
+        return render(request, 'meal_detail.html',
+                      {'meal': meal})

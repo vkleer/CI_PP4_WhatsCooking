fc94f574dd5eb56b283fbab08b623c4ceeb8bfde
Vilayat Kleer <vkleer@hotmail.com>
2022-09-19 22:17:08 +0000

    Update meal_planner page to include buttons for editing existing meal plans, update getCalendarCells function in meal-planner-calendar.js to set href of buttons to new EditMealPlan view

--- static/js/meal-planner-calendar.js
+++ static/js/meal-planner-calendar.js
@@ -3,11 +3,18 @@ function getCalendarCells() {
     cells = calendar.children
 
     for (i = 0; i < cells.length; i++) {
-        if (cells[i].children.length > 1 ) {
-            mealPlanButton = cells[i].querySelector('.btn');
-            mealPlanButton.innerHTML='Edit meal plan';
-            mealPlanButton.href='editView';
-            console.log(mealPlanButton);
+        mealPlanButton = cells[i].querySelector('.btn');
+        mealPlanId = cells[i].querySelector('input[name=meal_plan_id]')
+        console.log(mealPlanId)
+        if (cells[i].children.length > 2 ) {
+            cells[i].style.cssText = 'vertical-align:top';
+            mealPlanButton.innerHTML = 'Edit plan';
+            mealPlanButton.href = `edit_meal_plan/${mealPlanId.value}`;
+        }
+        else {
+            mealPlanButton.href = '/';
+            cells[i].style.cssText = 'vertical-align:center';
+            mealPlanButton.innerHTML = 'Create plan';
         }
     }
 }

e6d3a69e3a4948a787f66959af921a54d125ad11
Vilayat Kleer <vkleer@hotmail.com>
2022-09-21 01:17:39 +0000

    Rename meal-planner-calendar script to meal-planner, load meal-planner script in head, call functions in corresponding templates instead of always calling them on load

--- static/js/meal-planner.js
+++ static/js/meal-planner.js
@@ -5,17 +5,24 @@ function getCalendarCells() {
     for (i = 0; i < cells.length; i++) {
         mealPlanButton = cells[i].querySelector('.btn');
         mealPlanId = cells[i].querySelector('input[name=meal_plan_id]')
-        if (cells[i].children.length > 2 ) {
+        if (cells[i].children.length > 3 ) {
             cells[i].style.cssText = 'vertical-align:top';
             mealPlanButton.innerHTML = 'Edit plan';
             mealPlanButton.href = `edit_meal_plan/${mealPlanId.value}`;
         }
         else {
-            mealPlanButton.href = '/';
+            mealPlanDate = cells[i].querySelector('input[name=meal_plan_date]')
+            mealPlanButton.href = `create_meal_plan/${mealPlanDate.value}`;
             cells[i].style.cssText = 'vertical-align:center';
             mealPlanButton.innerHTML = 'Create plan';
         }
     }
 }
 
-getCalendarCells();
+function hideFormSelect() {
+    mealPlannerSel = document.getElementById('id_meal_planner')
+    dateSel = document.getElementById('id_date')
+
+    mealPlannerSel.setAttribute('hidden', '')
+    dateSel.setAttribute('hidden', '')
+}

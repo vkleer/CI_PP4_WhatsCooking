103a247ee58c8d6e16274865f569b29dac18cf0b
Vilayat Kleer <vkleer@hotmail.com>
2022-09-24 00:09:56 +0000

    Change Django message time-out to 4 seconds

--- static/js/message-dismissal.js
+++ static/js/message-dismissal.js
@@ -1,5 +1,7 @@
+// Sets a timer for Django messages - disappear after 4 seconds
+// Taken from 'I Think Therefore I Blog' project from Code Institute
 setTimeout(function () {
     let messages = document.getElementById('msg');
     let alert = new bootstrap.Alert(messages);
     alert.close();
-}, 2500);
+}, 4000);
